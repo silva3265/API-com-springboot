e3b09ea4e2c1f68d76ad0805ade47db8b1de1c71
Rômulo <silva3265@gmail.com>
2022-12-17 18:07:57 -0300

    Modelo

--- src/main/java/br/com/projeto/controle/Controle.java
+++ src/main/java/br/com/projeto/controle/Controle.java
@@ -1,6 +1,7 @@
 package br.com.projeto.controle;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +10,16 @@ public class Controle {
 	@GetMapping("")
 	public String mensagem() {
 		return "Hello Word";
+	}
+	
+	@GetMapping("/boasVindas")
+	public String boasVindas() {
+		return "Seja bem vindos(a)";
+	}
+	
+	@GetMapping("/boasVindas/{nome}")
+	public String boasVindas(@PathVariable String nome) {
+		return "Seja bem vindos(a)" + nome;
 	}
 
 }
