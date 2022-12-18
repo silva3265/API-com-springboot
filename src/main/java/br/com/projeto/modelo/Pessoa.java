6a3e62ff42004841c2289d356a1d63055a6cc9ca
Rômulo <silva3265@gmail.com>
2022-12-18 18:09:05 -0300

    Configurando modelo para gerar tabela

--- src/main/java/br/com/projeto/modelo/Pessoa.java
+++ src/main/java/br/com/projeto/modelo/Pessoa.java
@@ -1,15 +1,35 @@
 package br.com.projeto.modelo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Pessoa {
 	
 	//Atributo
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int codigo;
 	private String nome;
 	private int idade;
+	
 	
 	//Get e Set
 	public String getNome() {
 		return nome;
 	}
+	
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
