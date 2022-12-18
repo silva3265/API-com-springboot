4c44c85a3a5d853295105ce43a3722ec88ae9cc6
Rômulo <silva3265@gmail.com>
2022-12-18 19:14:39 -0300

    Efetuando cadastros com save()

--- src/main/java/br/com/projeto/controle/Controle.java
+++ src/main/java/br/com/projeto/controle/Controle.java
@@ -1,5 +1,6 @@
 package br.com.projeto.controle;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,11 +8,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.projeto.modelo.Pessoa;
+import br.com.projeto.repositorio.Repositorio;
 
 @RestController
 public class Controle {
 	
-	@GetMapping("")
+	@Autowired
+	private Repositorio acao;
+	
+	@PostMapping("/api")
+	public Pessoa cadastrar(@RequestBody Pessoa obj) {
+		return acao.save(obj);
+	}
+	
+	@GetMapping("") 
 	public String mensagem() {
 		return "Hello Word";
 	}
--- src/main/java/br/com/projeto/repositorio/Repositorio.java
+++ src/main/java/br/com/projeto/repositorio/Repositorio.java
@@ -3,9 +3,10 @@ package br.com.projeto.repositorio;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import br.com.projeto.modelo.Pessoa;
+
 @Repository
 public interface Repositorio extends CrudRepository<Pessoa, Integer>{
-	
 	
 
 }
