cbd598c45f4d1e18a1a7048517cb0680000c98d9
Rômulo <silva3265@gmail.com>
2022-12-18 20:12:22 -0300

    Listando dados com o comando findAll()

--- src/main/java/br/com/projeto/repositorio/Repositorio.java
+++ src/main/java/br/com/projeto/repositorio/Repositorio.java
@@ -1,5 +1,7 @@
 package br.com.projeto.repositorio;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,5 @@ import br.com.projeto.modelo.Pessoa;
 @Repository
 public interface Repositorio extends CrudRepository<Pessoa, Integer>{
 	
-
+	List<Pessoa> findAll(); //quando for chamado o findAll ele vai retornar uam lista de pessoas
 }
