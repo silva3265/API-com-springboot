1adaf43ea794941f97528819a7b5a9a0d7b6b1e5
Rômulo <silva3265@gmail.com>
2022-12-20 17:47:14 -0300

    Annotation @Component

---
+++ src/main/java/br/com/projeto/modelo/Mensagem.java
@@ -0,0 +1,20 @@
+package br.com.projeto.modelo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Mensagem {
+	
+	private String mensagem;
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	
+	
+
+}
--- src/main/java/br/com/projeto/servico/Servico.java
+++ src/main/java/br/com/projeto/servico/Servico.java
@@ -1,8 +1,14 @@
 package br.com.projeto.servico;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.projeto.modelo.Mensagem;
 
 @Service
 public class Servico {
+	
+	@Autowired
+	private Mensagem mensagem;
 
 }
